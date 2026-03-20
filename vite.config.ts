360a47aa24da73e0c6c3f339fd5690225f8e83bb
Pham Anh Tuan <nhutihome@gmail.com>
2026-03-20 13:11:22 +0700

    fix: clean vite config no legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
@@ -10,10 +9,6 @@ export default defineConfig(({mode}) => {
     plugins: [
       react(),
       tailwindcss(),
-      legacy({
-        targets: ['ios >= 13', 'safari >= 13'],
-        modernPolyfills: true,
-      }),
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
